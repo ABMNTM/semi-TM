cb594f2b918b07ad0911d5175f771440019da195
ABMNTM <mehrabiabol1383@gmail.com>
2024-03-28 18:40:18 +0330

    auth page added

---
+++ src/pages/auth.tsx
@@ -0,0 +1,13 @@
+import SignUp from "@cmp/forms/signup";
+
+import styles from "@sty/Home.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <div>
+        <SignUp />
+      </div>
+    </>
+  );
+}
--- src/pages/index.tsx
+++
@@ -1,8 +0,0 @@
-import { Inter } from "next/font/google";
-import styles from "@sty/Home.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
-
-export default function Home() {
-  return <></>;
-}
