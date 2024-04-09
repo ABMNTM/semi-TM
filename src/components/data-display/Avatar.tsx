40b340785b91d89b4539feb2afe9585622da8bef
ABMNTM <mehrabiabol1383@gmail.com>
2024-04-09 15:15:03 +0330

    added data-display and created Avatar and ProjectCard components

---
+++ src/components/data-display/Avatar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Image from "next/image";
+
+import styles from "./Avatar.module.css";
+
+const Avatar = () => {
+  return (
+    <>
+      <div className={styles.avatar}>AB</div>
+    </>
+  );
+};
+
+export default Avatar;
---
+++ src/components/data-display/ProjectCard.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import Avatar from "./Avatar";
+
+import styles from "./ProjectCard.module.css";
+
+interface propType {
+  title: string;
+}
+
+const ProjectCard = (props: propType) => {
+  const projectLink = "/a/#";
+  return (
+    <>
+      <Link className={styles.container} href={projectLink}>
+        <div className={styles.header}>
+          <FontAwesomeIcon icon={faStar} />
+          <h3>{props.title}</h3>
+        </div>
+        <div className={styles.body}>
+          <Avatar />
+        </div>
+      </Link>
+    </>
+  );
+};
+
+export default ProjectCard;
