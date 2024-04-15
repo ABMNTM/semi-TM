71aaa18e0d01037990c0aba9b7f2e7c86e228b0b
ABMNTM <mehrabiabol1383@gmail.com>
2024-04-15 15:29:21 +0330

    task page moves at dev status to repr. list card

--- src/pages/a/[projectID]/tasks.tsx
+++ src/pages/a/[projectID]/tasks.tsx
@@ -2,24 +2,54 @@ import React from "react";
 import { useRouter } from "next/router";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHouse } from "@fortawesome/free-solid-svg-icons";
+import ListCard, { taskType } from "@cmp/data-display/ListCard";
 
 import styles from "@sty/projectID/tasks.module.css";
+
+type tasks = Array<taskType>;
 
 interface propType {}
 
 const Page = (props: propType) => {
   const router = useRouter();
+  const dummyTasks: tasks = [
+    {
+      id: 1,
+      title: "nonono ...",
+      status: "TD",
+    },
+    {
+      id: 2,
+      title: "yeayeayea ...",
+      status: "DI",
+    },
+    {
+      id: 4,
+      title: "ohohoh ...",
+      status: "TD",
+    },
+    {
+      id: 3,
+      title: "hahaha ...",
+      status: "DN",
+    },
+  ];
   return (
-    <>
-      <div className={styles.container}>
-        <aside className={styles.aside}>
-          <div className={styles.home}>
-            تسکولو
-            <FontAwesomeIcon icon={faHouse} />
-          </div>
-        </aside>
-      </div>
-    </>
+    <ListCard title="testing" width={300} height={700} tasks={dummyTasks} />
+    // <>
+    //   <div className={styles.container}>
+    //     <aside className={styles.rightSide}>
+    //       <div className={styles.home}>
+    //         تسکولو
+    //         <FontAwesomeIcon icon={faHouse} />
+    //       </div>
+    //     </aside>
+    //     <div className={styles.leftSide}>
+    //       <nav className={styles.nav}></nav>
+    //       <main className={styles.taskContainer}>{/* tasks */}</main>
+    //     </div>
+    //   </div>
+    // </>
   );
 };
 
