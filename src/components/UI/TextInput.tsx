4a2dfe22220aa5192aa87d96e3ead9191a64c16f
mahdi <313mmy313@gmail.com>
2024-05-14 10:52:09 +0330

    create: TextInput comp. created according prevous SearchBar comp.

---
+++ src/components/UI/TextInput.tsx
@@ -0,0 +1,48 @@
+import React, { ChangeEvent, HTMLAttributes, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+
+import styles from "./TextInput.module.css";
+
+interface PropType {
+  placeholder?: string;
+  onChange(e: ChangeEvent<HTMLInputElement>): void;
+  Icon?: IconProp;
+}
+
+const TextInput = (props: PropType) => {
+  const [focused, setFocused] = useState(false);
+
+  const HandleFocus = () => {
+    setFocused(true);
+  };
+
+  const HandleBlur = () => {
+    setFocused(false);
+  };
+
+  return (
+    <>
+      <div
+        className={
+          styles.container + " " + (focused && styles.containerOnFocus)
+        }
+      >
+        <input
+          onFocus={HandleFocus}
+          onBlur={HandleBlur}
+          type="text"
+          name="search"
+          onChange={props.onChange}
+          id={styles.TextInput}
+          placeholder={props.placeholder}
+        />
+        {props.Icon && (
+          <FontAwesomeIcon icon={props.Icon} id={styles.magnify} />
+        )}
+      </div>
+    </>
+  );
+};
+
+export default TextInput;
